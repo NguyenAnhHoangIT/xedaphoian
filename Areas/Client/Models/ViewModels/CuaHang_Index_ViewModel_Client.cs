722d64706610dc144e08dd5ff903e1f43c681238
NguyenAnhHoangIT <nahoangit@gmail.com>
2025-06-05 19:37:56 +0700

    merge

--- Areas/Client/Models/ViewModels/CuaHang_Index_ViewModel_Client.cs
+++ Areas/Client/Models/ViewModels/CuaHang_Index_ViewModel_Client.cs
@@ -7,5 +7,6 @@
         public string MoTa { get; set; }
         public string HinhAnh { get; set; }
         public double DiemTrungBinh { get; set; }
+        public string DiaChi { get; set; }
     }
 }
